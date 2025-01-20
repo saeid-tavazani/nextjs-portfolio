b07ba45d852cb46b70808a156908993da72d6296
saeid-tavazani <saeidtavazani@gmail.com>
2025-01-20 21:52:09 +0330

    Add custom hook useHash for managing hash changes in URL

---
+++ hook/useHash.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+export const useHash = () => {
+  const [hash, setHash] = useState(window.location.hash);
+  useEffect(() => {
+    const onHashChange = () => {
+      setHash(window.location.hash);
+    };
+    window.addEventListener('hashchange', onHashChange);
+    return () => window.removeEventListener('hashchange', onHashChange);
+  }, []);
+
+  return hash;
+};
