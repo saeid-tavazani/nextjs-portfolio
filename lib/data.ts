5545b814d4314ba5f019e015a1f8925f7cefbc60
saeid-tavazani <saeidtavazani@gmail.com>
2025-01-21 22:36:12 +0330

    Add social media links with corresponding icons to data file

--- lib/data.ts
+++ lib/data.ts
@@ -2,6 +2,9 @@ import image2 from '@/assets/Card - Element-desktop (1).png';
 import image3 from '@/assets/Card - Element-desktop (2).png';
 import image4 from '@/assets/Card - Element-desktop (3).png';
 import image1 from '@/assets/Card - Element-desktop.png';
+import { FaGithub, FaPhone, FaSkype, FaTelegram } from 'react-icons/fa';
+import { FaInstagram, FaLinkedinIn } from 'react-icons/fa6';
+import { MdAlternateEmail } from 'react-icons/md';
 
 export const projects = [
   {
@@ -23,5 +26,43 @@ export const projects = [
     title: 'The Power of Communication',
     description: 'Understand the impact of effective communication in our lives.',
     image: image4,
+  },
+];
+
+export const links = [
+  {
+    slug: 'https://github.com/saeid-tavazani',
+    icon: FaGithub,
+    size: 40,
+  },
+  {
+    slug: 'https://join.skype.com/invite/HfZKgk1gjISo',
+    icon: FaSkype,
+    size: 40,
+  },
+  {
+    slug: 'https://www.instagram.com/saeed.tavazani/',
+    icon: FaInstagram,
+    size: 40,
+  },
+  {
+    slug: 'https://t.me/saeidIRs',
+    icon: FaTelegram,
+    size: 40,
+  },
+  {
+    slug: 'mailto:saeidtavazani@gmail.com',
+    icon: MdAlternateEmail,
+    size: 40,
+  },
+  {
+    slug: 'https://www.linkedin.com/in/saeidtavazani/',
+    icon: FaLinkedinIn,
+    size: 40,
+  },
+  {
+    slug: 'tel:09911561251',
+    icon: FaPhone,
+    size: 35,
   },
 ];
