6086e8c738275a727d1eaea4b8427382c525781c
saeid-tavazani <saeidtavazani@gmail.com>
2025-01-21 19:00:55 +0330

    Add projects data module with descriptions and images for project showcase

---
+++ lib/data.ts
@@ -0,0 +1,27 @@
+import image2 from '@/assets/Card - Element-desktop (1).png';
+import image3 from '@/assets/Card - Element-desktop (2).png';
+import image4 from '@/assets/Card - Element-desktop (3).png';
+import image1 from '@/assets/Card - Element-desktop.png';
+
+export const projects = [
+  {
+    title: 'The Dawn of Innovation',
+    description: 'Explore the birth of groundbreaking ideas and inventions.',
+    image: image1,
+  },
+  {
+    title: 'The Digital Revolution',
+    description: 'Dive into the transformative power of technology.',
+    image: image2,
+  },
+  {
+    title: 'The Art of Design',
+    description: 'Discover the beauty of thoughtful and functional design.',
+    image: image3,
+  },
+  {
+    title: 'The Power of Communication',
+    description: 'Understand the impact of effective communication in our lives.',
+    image: image4,
+  },
+];
